62a58e8f923152bf4f36f554d1cca250920692c0
Mamy32 <mamymanjakamanana32@gmail.com>
2026-06-01 13:14:24 +0700

    Backend connected to Aiven

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,23 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+require("dotenv").config();
+const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('MySQL Connected');
-    }
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log("Connected to MySQL");
 });
 
-module.exports = db;
+module.exports = connection;
